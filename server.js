f58bf86d8170d83f9af93605e1ee26a5ab8568d5
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-11-26 20:20:47 -0300

    up

---
+++ server.js
@@ -0,0 +1,3 @@
+const app = require('./venom');
+
+app.listen(3333);
---
+++ venom.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const mime = require('mime-types')
+const venom = require('venom-bot')
+const express = require('express');
+
+const app = express();
+
+
+app.use('/', express.static('public'));
+
+venom
+  .create(
+    'sessionName',
+    (base64Qr, asciiQR, attempts, urlCode) => {
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+ 
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+ 
+      var imageBuffer = response;
+      fs.writeFile(
+      './public/out.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    undefined,
+    { logQR: false }
+  )
+  .then((client) => {
+    start(client);
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage(async (message) => {
+    if (message.isMedia == true) {
+      const buffer = await client.decryptFile(message)
+      const fileName = `image.${mime.extension(message.mimetype)}`
+      await fs.writeFile(fileName, buffer, async (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('File written successfully')
+          await client.sendImageAsSticker(message.from, `./${fileName}`)
+            .catch((err) => { })
+        }
+      })
+    }
+    if (message.body === 'Hi') {
+      client.sendText(message.from, `Iae ${message.sender.shortName}`)
+    }
+  })
+}
+
+module.exports = app;
